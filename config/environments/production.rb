bd9d0e97364deda16f518eca68dc367671e19bd9
Mayowa Pitan <mayowa.pitan@andela.com>
2016-10-30 17:07:49 +0100

    edit allowed request origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://paitin-action-cable-chat-app.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://paitin-action-cable-chat-app.herokuapp.com/' ]
+  config.action_cable.allowed_request_origins = [ 'https://paitin-action-cable-chat-app.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
